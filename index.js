a0b9cd88c29b503d579e741250a94cd76fb93d59
Ahmed Ashraf <ahmedashraf@aucegypt.edu>
2021-01-14 15:56:52 +0200

    *E* None

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
     // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = 2468
+    let VERIFY_TOKEN = "2468"
       
     // Parse the query params
     let mode = req.query['hub.mode'];
