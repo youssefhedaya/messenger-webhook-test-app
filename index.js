f12010e5002fab74f8b96f5caa256299ae84f995
Ahmed Ashraf <ahmedashraf@aucegypt.edu>
2021-01-26 14:46:49 +0200

    jjj

--- index.js
+++ index.js
@@ -111,14 +111,10 @@ function handleMessage(sender_psid, received_message) {
   
   let response;
 
-  // Check if the message contains text
-  if (reformed_text) {    
-
     // Create the payload for a basic text message
     response = {
       "text": `You sent the message: "${reformed_text}". Now send me an image!`
     }
-  }  
   
   // Sends the response message
   callSendAPI(sender_psid, response);  
